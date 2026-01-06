4dc19a661399c70b2ecd79d6f1664e4511038899
kevinpaulitsch <kevin.paulitsch@gmail.com>
2026-01-06 18:11:27 +0100

    grammar.js

---
+++ grammar.js
@@ -0,0 +1,36 @@
+const grammar = {
+  1: [
+    { q: "___ the door, please.", a: "Open" },
+    { q: "Sit ___, please. (down / on)", a: "down" }
+  ],
+
+  2: [
+    { q: "The monkey is ___ the tree. (in / on / under)", a: "in" },
+    { q: "The lion is ___ the rock. (on / in)", a: "on" }
+  ],
+
+  3: [
+    { q: "He ___ got a ship. (has / have)", a: "has" },
+    { q: "They ___ pirates. (are / is)", a: "are" }
+  ],
+
+  4: [
+    { q: "It is ___ morning. (in / on)", a: "in" },
+    { q: "___ Monday we have school. (On / In)", a: "On" }
+  ],
+
+  5: [
+    { q: "She ___ sing. (can / can’t)", a: "can" },
+    { q: "He ___ play the guitar. (can / can’t)", a: "can" }
+  ],
+
+  6: [
+    { q: "He runs ___ the street. (down / in)", a: "down" },
+    { q: "The dog jumps ___ the river. (into / on)", a: "into" }
+  ],
+
+  7: [
+    { q: "I ___ eat meat. (never / always)", a: "never" },
+    { q: "We ___ have breakfast at 7. (usually / never)", a: "usually" }
+  ]
+};
