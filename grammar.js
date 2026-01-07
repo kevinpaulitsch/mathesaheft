aac755ed3a92e9bdfd6b9bc6602f393ab0b70581
kevinpaulitsch <kevin.paulitsch@gmail.com>
2026-01-07 14:26:03 +0100

    grammar.js

--- grammar.js
+++ grammar.js
@@ -1,93 +1,30 @@
 // grammar.js
-// Grammatik bewusst NICHT nach Units getrennt
-// Alle Aufgaben sind logisch eindeutig (keine Doppellösungen)
+// keine Mehrdeutigkeiten, immer genau eine richtige Lösung
 
-const GRAMMAR_TASKS = [
+const GRAMMAR_POOL = [
+  {q:"Fish live in water. They ___ swim.", a:"can", o:["can","can't"]},
+  {q:"Cats have no wings. They ___ fly.", a:"can't", o:["can","can't"]},
+  {q:"Birds have wings. They ___ fly.", a:"can", o:["can","can't"]},
+  {q:"She is two years old. She ___ drive a car.", a:"can't", o:["can","can't"]},
 
-  // CAN / CAN'T – eindeutige Fakten
-  () => ({
-    q: "Fish live in water. They ___ swim.",
-    options: ["can", "can't"],
-    a: "can"
-  }),
+  {q:"Tom ___ happy today.", a:"is", o:["is","are"]},
+  {q:"My friends ___ at school.", a:"are", o:["is","are"]},
 
-  () => ({
-    q: "Cats have no wings. They ___ fly.",
-    options: ["can", "can't"],
-    a: "can't"
-  }),
+  {q:"There ___ three apples on the table.", a:"are", o:["is","are"]},
+  {q:"There ___ a cat in the garden.", a:"is", o:["is","are"]},
 
-  () => ({
-    q: "She is two years old. She ___ drive a car.",
-    options: ["can", "can't"],
-    a: "can't"
-  }),
+  {q:"This is ___ apple.", a:"an", o:["a","an"]},
+  {q:"This is ___ dog.", a:"a", o:["a","an"]},
 
-  () => ({
-    q: "Birds have wings. They ___ fly.",
-    options: ["can", "can't"],
-    a: "can"
-  }),
-
-  // TO BE
-  () => ({
-    q: "Tom ___ happy today.",
-    options: ["is", "are"],
-    a: "is"
-  }),
-
-  () => ({
-    q: "My friends ___ at school.",
-    options: ["is", "are"],
-    a: "are"
-  }),
-
-  // THERE IS / ARE
-  () => ({
-    q: "There ___ three apples on the table.",
-    options: ["is", "are"],
-    a: "are"
-  }),
-
-  () => ({
-    q: "There ___ a cat in the garden.",
-    options: ["is", "are"],
-    a: "is"
-  }),
-
-  // A / AN – phonetisch eindeutig
-  () => ({
-    q: "This is ___ apple.",
-    options: ["a", "an"],
-    a: "an"
-  }),
-
-  () => ({
-    q: "This is ___ dog.",
-    options: ["a", "an"],
-    a: "a"
-  }),
-
-  // HAVE / HAS
-  () => ({
-    q: "She ___ a new bike.",
-    options: ["has", "have"],
-    a: "has"
-  }),
-
-  () => ({
-    q: "They ___ two dogs.",
-    options: ["has", "have"],
-    a: "have"
-  })
+  {q:"She ___ a new bike.", a:"has", o:["has","have"]},
+  {q:"They ___ two dogs.", a:"have", o:["has","have"]}
 ];
 
-// Randomisierte Abfrage
-function getGrammarTasks(amount = 20) {
-  const tasks = [];
-  for (let i = 0; i < amount; i++) {
-    const gen = GRAMMAR_TASKS[Math.floor(Math.random() * GRAMMAR_TASKS.length)];
-    tasks.push(gen());
+window.getGrammarTasks = function(amount=20){
+  const out=[];
+  for(let i=0;i<amount;i++){
+    const t=GRAMMAR_POOL[Math.floor(Math.random()*GRAMMAR_POOL.length)];
+    out.push({q:t.q, a:t.a, options:[...t.o]});
   }
-  return tasks;
-}
+  return out;
+};
